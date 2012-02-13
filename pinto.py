42e860031caa85a94646f3af4a970b9e89bb202b
Wayne Witzel III <wwitzel3@gmail.com>
2012-02-12 22:41:33 -0500

    Step 3, example of how the response body iterable works

--- pinto.py
+++ pinto.py
@@ -8,14 +8,24 @@ def application(environ, start_response):
     response_body = ['{0}: {1}'.format(key, val)
                      for key, val in sorted(environ.items())]
     response_body = '\n'.join(response_body)
+
+    # response body is an interable, each item is returns from the
+    # serve. be sure to always wrap single responses to avoid
+    # iterating over every character in a single response string
+
+    response_body = [
+        '*' * 30 + '\n',
+        response_body + '\n',
+        '*' * 30 + '\n',
+    ]
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'text/plain'),
-        ('Content-Length', str(len(response_body)))
+        ('Content-Length', str(sum(len(i) for i in response_body)))
     ]
 
     start_response(status, response_headers)
-    return [response_body]
+    return response_body
 
 def main():
     httpd = make_server('localhost', 8051, application)
