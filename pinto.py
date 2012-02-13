560d68c3ecae5f5c2e9e72adee54ff3f3078c20a
Wayne Witzel III <wwitzel3@gmail.com>
2012-02-12 23:17:20 -0500

    Step 4, add a template system and handle a form get request

--- pinto.py
+++ pinto.py
@@ -1,26 +1,40 @@
+import os
+
 # My random WSGI playings around.
 from wsgiref.simple_server import make_server
+
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+from cgi import parse_qs
+from cgi import escape
 
 # This is called application because if you want to use this
 # with mod_wsgi it is requied to be named application
 
 def application(environ, start_response):
-    response_body = ['{0}: {1}'.format(key, val)
-                     for key, val in sorted(environ.items())]
-    response_body = '\n'.join(response_body)
+    here = os.getcwd()
 
+    # setup our mako stuff and load index.html
+    template_path = os.path.join(here,'templates')
+    lookup = TemplateLookup(directories=[template_path])
+    template = Template(filename=os.path.join(template_path,'index.html'))
+
+    d = parse_qs(environ['QUERY_STRING'])
+    args = dict(
+        age=escape(d.get('age',[''])[0]),
+        name=escape(d.get('name',[''])[0]),
+        cb=[escape(c) for c in d.get('cb',[])]
+    )
+
+    response_body = [str(template.render(**args))]
     # response body is an interable, each item is returns from the
     # serve. be sure to always wrap single responses to avoid
     # iterating over every character in a single response string
 
-    response_body = [
-        '*' * 30 + '\n',
-        response_body + '\n',
-        '*' * 30 + '\n',
-    ]
     status = '200 OK'
     response_headers = [
-        ('Content-Type', 'text/plain'),
+        ('Content-Type', 'text/html'),
         ('Content-Length', str(sum(len(i) for i in response_body)))
     ]
 
@@ -29,7 +43,7 @@ def application(environ, start_response):
 
 def main():
     httpd = make_server('localhost', 8051, application)
-    httpd.handle_request()
+    httpd.serve_forever()
 
 if __name__ == '__main__':
     main()
