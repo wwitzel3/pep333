276aff209c223f312fb326e11eed9f9c99aebe92
Wayne Witzel III <wwitzel3@gmail.com>
2012-02-12 22:30:30 -0500

    Step 2, add a serve component and make the response have something interesting

--- pinto.py
+++ pinto.py
@@ -1,9 +1,13 @@
 # My random WSGI playings around.
+from wsgiref.simple_server import make_server
+
 # This is called application because if you want to use this
 # with mod_wsgi it is requied to be named application
 
 def application(environ, start_response):
-    response_body = 'Request method %s' % environ['REQUEST_METHOD']
+    response_body = ['{0}: {1}'.format(key, val)
+                     for key, val in sorted(environ.items())]
+    response_body = '\n'.join(response_body)
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'text/plain'),
@@ -12,4 +16,11 @@ def application(environ, start_response):
 
     start_response(status, response_headers)
     return [response_body]
+
+def main():
+    httpd = make_server('localhost', 8051, application)
+    httpd.handle_request()
+
+if __name__ == '__main__':
+    main()
 
